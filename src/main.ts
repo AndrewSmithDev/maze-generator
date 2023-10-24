79f1a910fe7f4c7df5707d664a7573048b1ada35
Andrew Smith <contact@andrewsmith.dev>
2023-10-23 23:29:37 -0400

    fix performance
    clear graphics after each render

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import { graphics } from "./core/graphics";
 import { setupPIXIApplication } from "./core/pixi-setup";
 import { Grid } from "./entities/grid";
 
-const height = 601; // plus one for the border
-const width = 601; // plus one for the border
-const cellSize = 40;
+const height = 801; // plus one for the border
+const width = 801; // plus one for the border
+const cellSize = 20;
 
 const app = setupPIXIApplication({ height, width, graphics, background: 0x222222 });
 
 const grid = new Grid({ cellSize, height, width });
 
 app.ticker.add(() => {
+  graphics.clear();
   grid.draw();
-  grid.depthFirstSearch();
 });
+
+setInterval(() => {
+  grid.depthFirstSearch();
+}, 1000 / 120);
